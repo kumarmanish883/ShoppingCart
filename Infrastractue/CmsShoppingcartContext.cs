4b60a6093ea78dbff13a37676d112bfc88ba894f
Manish kumar shah <me.lekha7759@gmail.com>
2022-01-10 11:20:21 +0545

    Dbcontext

---
+++ Infrastractue/CmsShoppingcartContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Infrastractue
+{
+    public class CmsShoppingcartContext:DbContext
+    {
+        public CmsShoppingcartContext(DbContextOptions<CmsShoppingcartContext>options)
+            :base(options)
+         {
+
+        }
+    }
+}
