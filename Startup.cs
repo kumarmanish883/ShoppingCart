64fab32d6c30085a8288f0709e1084117688fe86
Manish kumar shah <me.lekha7759@gmail.com>
2022-01-10 19:54:23 +0545

    Admin area and Pages controller 56:24 Admin Pages Index method

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,11 @@ namespace ShoppingCart
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+              name: "areas",
+              pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
