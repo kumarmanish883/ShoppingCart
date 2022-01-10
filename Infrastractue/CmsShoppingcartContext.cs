5998a93595030f9ce1b60bea47710744b44a40b1
Manish kumar shah <me.lekha7759@gmail.com>
2022-01-10 12:00:30 +0545

    Page model

--- Infrastractue/CmsShoppingcartContext.cs
+++ Infrastractue/CmsShoppingcartContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace ShoppingCart.Infrastractue
          {
 
         }
+        public DbSet<Page> pages { get; set; }
     }
 }
