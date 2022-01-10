58e2fe1e4acb6b7067e2abf51d9c7ec2abeddaa1
Manish kumar shah <me.lekha7759@gmail.com>
2022-01-10 12:22:53 +0545

    Initial migration

--- Models/Page.cs
+++ Models/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace ShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Slug { get; set; }
+        [Required]
         public string Content { get; set; }
+        [Required]
         public int Sorting { get; set; }
          
     }
