01e8e461289f3287ed50ecccaa4190a3ccc1bd91
Manish kumar shah <me.lekha7759@gmail.com>
2022-01-17 10:34:43 +0545

    installing jqueryui, sorting page1 and page 2 Httppost method for reorder is added in controller

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -121,5 +121,24 @@ namespace ShoppingCart.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+        //Post/Admin/Pages/Reorder
+        [HttpPost]
+       
+        public async Task<IActionResult> Reorder(int[] id)
+        {
+            int count = 1;
+            foreach (var pageId in id)
+            {
+                Page page = await context.pages.FindAsync(pageId);
+                page.Sorting = count;
+                context.Update(page);
+                await context.SaveChangesAsync();
+                count++;
+
+
+            }
+            return Ok();
+        }
     }
 }
